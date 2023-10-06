3bb1c0d1018268888d0162a4289bdc5a74232415
eejonghwa <teriyaki970326@gamil.com>
2023-10-06 16:09:23 +0900

    search 없ìê²  ãsearch isEmpty

---
+++ src/main/java/test/Article.java
@@ -0,0 +1,35 @@
+package test;
+
+public class Article {
+    private int id;
+    private String title;
+    private String content;
+
+    public Article(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(){
+        this.id = id;
+    }
+}
